ee56ef0a2ba3172790fbcbfa3605538d8c050ab8
namiya <hfweicheng@outlook.com>
2021-12-05 13:29:48 +0800

    增加对于连接错误的捕获，增加网络错误时的url日志显示

--- src/plugins/hxzxhelper/lib/utils.py
+++ src/plugins/hxzxhelper/lib/utils.py
@@ -27,16 +27,18 @@ async def get_advanced(url: str, params=None, headers=None, proxies=None) -> Uni
                     ret.raise_for_status()
                 return ret
             except httpx.HTTPStatusError:
-                logger.warning(f"服务器状态码错误：{ret.status_code}")
+                logger.warning(f"服务器状态码错误：{ret.status_code}, url='{url}'")
             except httpx.ConnectTimeout:
-                logger.warning("服务器连接超时")
+                logger.warning(f"服务器连接超时, url='{url}'")
+            except httpx.ConnectError:
+                logger.warning(f"服务器连接错误, url='{url}'")
             except httpx.ReadTimeout:
-                logger.warning("服务器读取超时")
+                logger.warning(f"服务器读取超时, url='{url}'")
             except httpx.ProxyError:
-                logger.warning("代理服务器出错")
+                logger.warning(f"代理服务器出错, url='{url}'")
             except httpx.RequestError:
-                logger.exception("网络错误")
+                logger.exception(f"网络错误, url='{url}'")
             await asyncio.sleep(0.3)
         else:
-            logger.error("所有Get尝试均失败，返回None")
+            logger.error(f"所有Get尝试均失败，返回None, url='{url}'")
             return None
